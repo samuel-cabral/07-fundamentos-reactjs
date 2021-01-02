744305671a5db5f6f88c4862b68d294c8fa1f7b7
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-01-01 23:12:48 -0300

    sparkle: challenge finalized

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles) return;
 
+    const droppedFile = uploadedFiles[0];
+
+    data.append('file', droppedFile.file, droppedFile.name);
+
+    history.push('/');
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadedFile = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadedFile);
   }
 
   return (
